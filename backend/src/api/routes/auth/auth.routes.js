ab4322c4e10243b73bad97ff22422cb5460c3861
Sagar Moriya <sagarmaurya814@gmail.com>
2024-05-05 12:57:01 +0530

    added user validation middleware

--- backend/src/api/routes/auth/auth.routes.js
+++ backend/src/api/routes/auth/auth.routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
-import { registerUser } from "../../controllers/auth/auth.controller.js"
+import { registerUser } from "../../controllers/auth/auth.controller.js";
+import validateBody from '../../../middlewares/validateBody.js';
+import userSchemaValidator from '../../../validators/userSchemaValidator.js';
+
 
 const router = express.Router();
 
 
-router.route('/register').post(registerUser);
+router.route('/register').post(validateBody(userSchemaValidator), registerUser);
 
 export default router;
