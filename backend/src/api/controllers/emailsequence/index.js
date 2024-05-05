8d624a13e5bddb3fccd2909d57718b0ad3540385
Sagar Moriya <sagarmaurya814@gmail.com>
2024-05-05 20:25:18 +0530

    integrated fetching single sequence

--- backend/src/api/controllers/emailsequence/index.js
+++ backend/src/api/controllers/emailsequence/index.js
@@ -1,3 +1,4 @@
 export { default as createEmailSequence } from './create.emailSequence.controller.js';
 export { default as deleteEmailSequence } from './delete.emailSequence.controller.js';
-export { default as getAllEmailSequence } from './getAll.emailSequence.controller.js'
+export { default as getAllEmailSequence } from './getAll.emailSequence.controller.js';
+export { default as getEmailSequence } from './getEmailSequence.controller.js'
--- backend/src/api/routes/emailsequence/emailsequence.routes.js
+++ backend/src/api/routes/emailsequence/emailsequence.routes.js
@@ -3,12 +3,14 @@ import express from 'express';
 import validateBody from '../../../middlewares/validateBody.js';
 import emailSequenceSchemaValidator from '../../../validators/emailSequenceValidator.js';
 import authenticateUserByToken from '../../../middlewares/authenticateUserByToken.js';
-import { createEmailSequence, deleteEmailSequence, getAllEmailSequence } from '../../controllers/emailsequence/index.js'
+import { createEmailSequence, deleteEmailSequence, getAllEmailSequence, getEmailSequence } from '../../controllers/emailsequence/index.js'
 
 
 const router = express.Router();
 
 router.route('/').get(authenticateUserByToken, getAllEmailSequence)
+
+router.route('/:id').get(authenticateUserByToken, getEmailSequence)
 
 router.route('/').post(authenticateUserByToken, validateBody(emailSequenceSchemaValidator), createEmailSequence);
 
