522524508bffc28f0c4ab6fb096f15c354ed42db
Sagar Moriya <sagarmaurya814@gmail.com>
2024-05-05 20:04:58 +0530

    integrated fetching all email sequence functionality

--- backend/src/api/controllers/emailsequence/index.js
+++ backend/src/api/controllers/emailsequence/index.js
@@ -1,2 +1,3 @@
 export { default as createEmailSequence } from './create.emailSequence.controller.js';
-export { default as deleteEmailSequence } from './delete.emailSequence.controller.js'
+export { default as deleteEmailSequence } from './delete.emailSequence.controller.js';
+export { default as getAllEmailSequence } from './getAll.emailSequence.controller.js'
