b6cc16358fb6e2400c2b308f96fffb06b2a7d1d4
Sagar Moriya <sagarmaurya814@gmail.com>
2024-05-08 18:24:29 +0530

    updated packages

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,27 @@ import { mongoDBCon } from './src/config/mongoDBCon.js';
 import authRoutes from './src/api/routes/auth/auth.routes.js';
 import emailSequenceRoutes from './src/api/routes/emailsequence/emailsequence.routes.js';
 import cors from 'cors';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url'
 
 
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRoutes)
-app.use('/api/emailsequence', emailSequenceRoutes)
+app.use('/api/emailsequence', emailSequenceRoutes);
+
+
+// to run on production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 const restart = () => mongoDBCon().then(() => {
     console.log('mongodb connected');
